d38f0d19ac7598c48ab4df74c0a17d13edc2a940
Raghav <raghav297@gmail.com>
2014-04-04 19:11:53 -0700

    FB Login v1.0

--- TellMeNow/TellMeNow/tellmenowAppDelegate.m
+++ TellMeNow/TellMeNow/tellmenowAppDelegate.m
@@ -7,15 +7,30 @@
 //
 
 #import "tellmenowAppDelegate.h"
+#import <FacebookSDK/FacebookSDK.h>
 
 @implementation tellmenowAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [FBLoginView class];
     // Override point for customization after application launch.
     return YES;
 }
-							
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation {
+    
+    // Call FBAppCall's handleOpenURL:sourceApplication to handle Facebook app responses
+    BOOL wasHandled = [FBAppCall handleOpenURL:url sourceApplication:sourceApplication];
+    
+    // You can add your app-specific url handling code here if needed
+    
+    return wasHandled;
+}
+
 - (void)applicationWillResignActive:(UIApplication *)application
 {
     // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- TellMeNow/TellMeNow/tellmenowViewController.m
+++ TellMeNow/TellMeNow/tellmenowViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "tellmenowViewController.h"
+# import <FacebookSDK/FacebookSDK.h>
 
 @interface tellmenowViewController ()
 
@@ -14,16 +15,7 @@
 
 @implementation tellmenowViewController
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
