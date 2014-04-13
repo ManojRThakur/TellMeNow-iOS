61f561082f176dbe8006e5f24df980f1e05acd54
Raghav <raghav297@gmail.com>
2014-04-12 17:53:16 -0700

    added stuff

---
+++ TellMeNow/TellMeNow/NotifCommentTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  NotifCommentTableViewCell.h
+//  TellMeNow
+//
+//  Created by Raghav on 4/12/14.
+//  Copyright (c) 2014 House Boelter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotifCommentTableViewCell : UITableViewCell
+
+@end
