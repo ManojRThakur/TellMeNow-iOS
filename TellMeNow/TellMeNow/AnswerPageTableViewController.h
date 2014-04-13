d991648612b4ed40fd9edbe484b882f40c3b350d
Raghav <raghav297@gmail.com>
2014-04-12 18:28:40 -0700

    contains all page drafts

---
+++ TellMeNow/TellMeNow/AnswerPageTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AnswerPageTableViewController.h
+//  TellMeNow
+//
+//  Created by Raghav on 4/12/14.
+//  Copyright (c) 2014 House Boelter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnswerPageTableViewController : UITableViewController
+
+@end
---
+++ TellMeNow/TellMeNow/ProfileTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileTableViewController.h
+//  TellMeNow
+//
+//  Created by Raghav on 4/12/14.
+//  Copyright (c) 2014 House Boelter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileTableViewController : UITableViewController
+
+@end
